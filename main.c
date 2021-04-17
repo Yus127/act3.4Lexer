082f8ff77df52d96d6e501b07331224ec18593f0
Yus127 <48135979+Yus127@users.noreply.github.com>
2021-04-16 22:16:28 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
+//codigo de ejemplo   ~ ! # $ %  ^ & * ( ) _ + | \ - = { } [ ] : ; < > ? , . /
 #include <stdio.h>
+struct person
+{
+   int age;
+   float weight;
+};
 
-int main(void) {
+int main()
+{
+    struct person *personPtr, person1;
+    int numero = 4.5e3;
+    int a = 2 + 4 * 7303;
+    personPtr = &person1;
+    /*
+    Un comentario
+    */
+    printf("Enter age: ");
+    scanf("%d", &personPtr->age);
 
-  for (int i = 0; i < 5; ++i) {
-     printf("C programming");
-  }
-  <= >= ==
+    printf("Enter weight: ");
+    scanf("%f", &personPtr->weight);
 
- // Error: i is not declared at this point
-  printf("%d", i);
-  return 0;
+    printf("Displaying:\n");
+    printf("Age: %d\n", personPtr->age);
+    printf("weight: %f", personPtr->weight);
+
+    return 0;
 }
-    ~ ! # $ %  ^ & * ( ) _ + | \ - = { } [ ] : ; < > ? , . /
