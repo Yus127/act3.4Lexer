19d91c57c328f7d25aeeeac96720fd68a7425d32
Yusdivia Molina <yus.molina@gmail.com>
2021-06-01 12:00:27 -0500

    creo ya esta

--- c/main.c
+++ c/main.c
@@ -1,29 +1,33 @@
-//codigo de ejemplo   ~ ! # $ %  ^ & * ( ) _ + | \ - = { } [ ] : ; < > ? , . /
 #include <stdio.h>
-struct person
-{
-   int age;
-   float weight;
-};
 
-int main()
-{
-    struct person *personPtr, person1;
-    int numero = 4.5e3;
-    int a = 2 + 4 * 7303;
-    personPtr = &person1;
-    /*
-    Un comentario
-    */
-    printf("Enter age: ");
-    scanf("%d", &personPtr->age);
+int main() {
+  /* TODO: declare the 2D array grades here */
+  float average;
+  int i;
+  int j;
 
-    printf("Enter weight: ");
-    scanf("%f", &personPtr->weight);
+  grades[0][0] = 80;
+  grades[0][1] = 70;
+  grades[0][2] = 65;
+  grades[0][3] = 89;
+  grades[0][4] = 90;
 
-    printf("Displaying:\n");
-    printf("Age: %d\n", personPtr->age);
-    printf("weight: %f", personPtr->weight);
+  grades[1][0] = 85;
+  grades[1][1] = 80;
+  grades[1][2] = 80;
+  grades[1][3] = 82;
+  grades[1][4] = 87;
 
-    return 0;
+  /* TODO: complete the for loop with appropriate terminating conditions */
+  for (i = 0; i < ; i++) {
+    average = 0;
+    for (j = 0; j < ; j++) {
+      average += grades[i][j];
+    }
+
+    /* TODO: compute the average marks for subject i */
+    printf("The average marks obtained in subject %d is: %.2f\n", i, average);
+  }
+
+  return 0;
 }
--- main.erl
+++ main.erl
@@ -4,7 +4,9 @@
 %Fecha de modificación: 16/04/2021
 
 -module(main).
--export([scan/1]).
+-export([scan/1, corre/0]).
+
+corre() -> timer:tc(?MODULE, scan, [["a","b","c"]]).
 
 %Función principal, recibe el nombre del archivo por leer y escribe la salida sobre "salida.html", usa el archivo "header.html" como base
 scan([]) -> ok;
@@ -21,6 +23,33 @@ scan([H|T]) ->
   io:format(Device2,"~n~s~n~s~n", ["</body>", "</html>"]),
   file:close(Device2),
   scan(T).
+
+principal([])-> ok;
+principal([H|T])->
+Pid = spawn(?MODULE,loop3,[]),
+  Pid ! {sumado, self(), H},
+  principal(T).
+
+loop3() ->
+  receive
+    {sumado, Pid, FileN} ->
+
+    {ok, Content} = file:read_file(filename:join([FileN, "main.c"])),
+    Lst = binary_to_list(Content),
+    {ok, Lst2, _} = lexer:string(Lst),
+    {ok,Device2} = file:open(filename:join([FileN, "salida.html"]), write),
+    {ok, Content2} = file:read_file("header.html"),
+    io:format(Device2, "~s", [binary_to_list(Content2)]),
+    io:format(Device2,"~s~n", ["<body>"]),
+    write(Lst2, Device2),
+    io:format(Device2,"~n~s~n~s~n", ["</body>", "</html>"]),
+    file:close(Device2)
+
+
+    end.
+
+
+
 
 %Función de apoyo para scan, se encarga de escribir sobre el archivo dado
 write([], _) -> true;
