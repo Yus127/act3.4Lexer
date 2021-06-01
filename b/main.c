1065ffdba01e7e6cf6aa9dd3746c694615af593f
Yusdivia Molina <yus.molina@gmail.com>
2021-06-01 12:17:48 -0500

    almost there

--- b/main.c
+++ b/main.c
@@ -4,3 +4,5 @@ int main() {
   printf("Goodbye, World!");
   return 0;
 }
+04894 +4342
+return
--- main.erl
+++ main.erl
@@ -4,32 +4,17 @@
 %Fecha de modificación: 16/04/2021
 
 -module(main).
--export([corre/0,loop3/0]).
+-export([corre/0,loop3/0, reunir/1, principal/2]).
 
-%corre() -> timer:tc(?MODULE, principal, [["a","b","c"]]).
-corre() -> principal(["a","b","c"]).
+corre() -> timer:tc(?MODULE, principal, [["a","b","c"],3]).
+%corre() -> principal(["a","b","c"],3).
 
-%Función principal, recibe el nombre del archivo por leer y escribe la salida sobre "salida.html", usa el archivo "header.html" como base
-scan([]) -> ok;
-scan([H|T]) ->
-  %{ok} = file:cd(Dir),
-  {ok, Content} = file:read_file(filename:join([H, "main.c"])),
-  Lst = binary_to_list(Content),
-  {ok, Lst2, _} = lexer:string(Lst),
-  {ok,Device2} = file:open(filename:join([H, "salida.html"]), write),
-  {ok, Content2} = file:read_file("header.html"),
-  io:format(Device2, "~s", [binary_to_list(Content2)]),
-  io:format(Device2,"~s~n", ["<body>"]),
-  write(Lst2, Device2),
-  io:format(Device2,"~n~s~n~s~n", ["</body>", "</html>"]),
-  file:close(Device2),
-  scan(T).
 
-principal([])-> ok;
-principal([H|T])->
+principal([],N)-> reunir(N);
+principal([H|T],N)->
   Pid = spawn(?MODULE,loop3,[]),
   Pid ! {sumado, self(), H},
-  principal(T).
+  principal(T,N).
 
 loop3() ->
   receive
@@ -43,7 +28,15 @@ loop3() ->
     io:format(Device2,"~s~n", ["<body>"]),
     write(Lst2, Device2),
     io:format(Device2,"~n~s~n~s~n", ["</body>", "</html>"]),
-    file:close(Device2)
+    file:close(Device2),
+    Pid ! {resul}
+    end.
+
+  reunir(0) -> 0;
+  reunir(N) ->
+      receive
+        {resul} ->
+          reunir(N-1)
     end.
 
 
