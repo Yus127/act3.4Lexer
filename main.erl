f443e196bc394ccffa07f18e27924078f13cf247
Yusdivia Molina <yus.molina@gmail.com>
2021-06-01 11:28:56 -0500

    varias carpetas

--- main.erl
+++ main.erl
@@ -7,17 +7,20 @@
 -export([scan/1]).
 
 %Función principal, recibe el nombre del archivo por leer y escribe la salida sobre "salida.html", usa el archivo "header.html" como base
-scan(File1) ->
-  {ok, Content} = file:read_file(File1),
+scan([]) -> ok;
+scan([H|T]) ->
+  %{ok} = file:cd(Dir),
+  {ok, Content} = file:read_file(filename:join([H, "main.c"])),
   Lst = binary_to_list(Content),
   {ok, Lst2, _} = lexer:string(Lst),
-  {ok,Device2} = file:open("salida.html", write),
+  {ok,Device2} = file:open(filename:join([H, "salida.html"]), write),
   {ok, Content2} = file:read_file("header.html"),
   io:format(Device2, "~s", [binary_to_list(Content2)]),
   io:format(Device2,"~s~n", ["<body>"]),
   write(Lst2, Device2),
   io:format(Device2,"~n~s~n~s~n", ["</body>", "</html>"]),
-  file:close(Device2).
+  file:close(Device2),
+  scan(T).
 
 %Función de apoyo para scan, se encarga de escribir sobre el archivo dado
 write([], _) -> true;
