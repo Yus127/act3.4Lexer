137a683fcfd3275e0ad7f4eab00291274c929e5b
Yusdivia Molina <yus.molina@gmail.com>
2021-06-01 12:20:13 -0500

    same main

--- a/main.c
+++ a/main.c
@@ -1,90 +1,29 @@
 //codigo de ejemplo   ~ ! # $ %  ^ & * ( ) _ + | \ - = { } [ ] : ; < > ? , . /
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct node
+struct person
 {
-  int val;
-  struct node * left;
-  struct node * right;
-} node_t;
-
-void insert(node_t * tree,int val);
-void print_tree(node_t * current);
-void printDFS(node_t * current);
+   int age;
+   float weight;
+};
 
 int main()
 {
-  node_t * test_list = (node_t *) malloc(sizeof(node_t));
+    struct person *personPtr, person1;
+    int numero = 4.5e3;
+    int a = 2 + 4 * 7303;
+    personPtr = &person1;
+    /*
+    Un comentario
+    */
+    printf("Enter age: ");
+    scanf("%d", &personPtr->age);
 
-  /* set values explicitly, alternative would be calloc() */
+    printf("Enter weight: ");
+    scanf("%f", &personPtr->weight);
 
-  test_list->val = 0;
-  test_list->left = NULL;
-  test_list->right = NULL;
+    printf("Displaying:\n");
+    printf("Age: %d\n", personPtr->age);
+    printf("weight: %f", personPtr->weight);
 
-  insert(test_list,5);
-  insert(test_list,8);
-  insert(test_list,4);
-  insert(test_list,3);
-
-  printDFS(test_list);
-  printf("\n");
-}
-
-void insert(node_t * tree, int val)
-{
-  if (tree->val == 0)
-  {
-    /* insert on current (empty) position */
-    tree->val = val;
-  }
-  else
-  {
-    if (val < tree->val)
-    {
-      /* insert left */
-      if (tree->left != NULL)
-      {
-        insert(tree->left, val);
-      }
-      else
-      {
-        tree->left = (node_t *) malloc(sizeof(node_t));
-        /* set values explicitly, alternative would be calloc() */
-        tree->left->val = val;
-        tree->left->left = NULL;
-        tree->left->right = NULL;
-      }
-    }
-    else
-    {
-      if (val >= tree->val)
-      {
-        /* insert right */
-        if (tree->right != NULL)
-        {
-          insert(tree->right,val);
-        }
-        else
-        {
-          tree->right = (node_t *) malloc(sizeof(node_t));
-          /* set values explicitly, alternative would be calloc() */
-          tree->right->val = val;
-          tree->right->left = NULL;
-          tree->right->right = NULL;
-        }
-      }
-    }
-  }
-}
-
-/* depth-first search */
-void printDFS(node_t * current)
-{
-  /* change the code here */
-  if (current == NULL)         return;   /* security measure */
-  if (current->left != NULL)   printDFS(current->left);
-  if (current != NULL)         printf("%d ", current->val);
-  if (current->right != NULL)  printDFS(current->right);
+    return 0;
 }
--- c/main.c
+++ c/main.c
@@ -1,4 +1,29 @@
+//codigo de ejemplo   ~ ! # $ %  ^ & * ( ) _ + | \ - = { } [ ] : ; < > ? , . /
+#include <stdio.h>
+struct person
+{
+   int age;
+   float weight;
+};
 
-abc
-/
-//ahd
+int main()
+{
+    struct person *personPtr, person1;
+    int numero = 4.5e3;
+    int a = 2 + 4 * 7303;
+    personPtr = &person1;
+    /*
+    Un comentario
+    */
+    printf("Enter age: ");
+    scanf("%d", &personPtr->age);
+
+    printf("Enter weight: ");
+    scanf("%f", &personPtr->weight);
+
+    printf("Displaying:\n");
+    printf("Age: %d\n", personPtr->age);
+    printf("weight: %f", personPtr->weight);
+
+    return 0;
+}
