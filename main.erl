a31f221019c80823f2374124b14f04880b4ecc6c
Yus127 <48135979+Yus127@users.noreply.github.com>
2021-06-03 10:54:33 -0500

    Update main.erl

--- main.erl
+++ main.erl
@@ -1,33 +1,9 @@
 %Programa que recibe lee un archivo en lenguaje c (.c) y resalta las categorías léxicas en un HTML
 %Yusdivia Molina Román A01653120
 %Lidia Paola Díaz Ramírez A01369117
-%Fecha de modificación: 16/04/2021
+%Fecha de modificación: /2021
 
 -module(main).
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
--export([scan/1,corre/0]).
-
-corre() -> timer:tc(?MODULE, scan, [["a","b","c"]]).
-
-%Función principal, recibe el nombre del archivo por leer y escribe la salida sobre "salida.html", usa el archivo "header.html" como base
-scan([]) -> 0;
-scan([H|T]) ->
-  %{ok} = file:cd(Dir),
-  {ok, Content} = file:read_file(filename:join([H, "main.c"])),
-  Lst = binary_to_list(Content),
-  {ok, Lst2, _} = lexer:string(Lst),
-  {ok,Device2} = file:open(filename:join([H, "salida.html"]), write),
-  {ok, Content2} = file:read_file("header.html"),
-  io:format(Device2, "~s", [binary_to_list(Content2)]),
-  io:format(Device2,"~s~n", ["<body>"]),
-  write(Lst2, Device2),
-  io:format(Device2,"~n~s~n~s~n", ["</body>", "</html>"]),
-  file:close(Device2),
-  scan(T).
-=======
-=======
->>>>>>> Stashed changes
 -export([corre/0,loop3/0, reunir/1, principal/2, corre2/0,scan/1]).
 
 corre() -> timer:tc(?MODULE, principal, [["a","b","c"],3]).
@@ -76,14 +52,8 @@ loop3() ->
       io:format(Device2,"~n~s~n~s~n", ["</body>", "</html>"]),
       file:close(Device2),
       scan(T).
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
 
 
-=======
-
-
->>>>>>> Stashed changes
 %Función de apoyo para scan y loop3, se encarga de escribir sobre el archivo dado
 write([], _) -> true;
 write([H | T], Device2) ->
